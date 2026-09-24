8227125799e2f17a41614c443e7eda6d45ec7a3b
Houssam <houssamelbadaoui5@gmail.com>
2026-09-24 18:32:12 +0200

    Add function to update UI

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 // select button
 const button = document.querySelector(".submit");
+// select tasks container
+const container = document.querySelector(".container");
 
 // array for tasks
 const tasks = [];
 
+
+// function that update the UI
+function updateUI(){
+  tasks.forEach(task => {
+    // create a div 
+    const card = document.createElement("div");
+
+    card.innerHTML = `
+    <h3 class="task-header">${task.title}</h3>
+    <p class="category">${task.category}</p>
+    <p class="priority">${task.priority}</p>
+    <p class="duration">${task.duration}</p>
+    <div class="check-group">
+    <input type="checkbox" id="${task.id}" name="completed" value="comleted">
+    <label for="${task.id} > Completed </label>
+    </div>
+    `
+
+    container.appendChild(card);
+  })
+}
 // create a class for task
 class Task {
   constructor(title, category, priority, duration) {
